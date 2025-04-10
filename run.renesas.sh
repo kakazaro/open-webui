bc6a7cca48b22cd9e353c21f675b0dba4db58c19
quanle <quan.le.xf@renesas.com>
2025-04-10 11:39:39 +0700

    create deploy script

--- run.renesas.sh
+++ run.renesas.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-image_name="kakazaro/open-webui:latest"
-
 git pull
 
-docker build -f Dockerfile.renesas -t "$image_name" .
-docker push "$image_name"
-
-docker compose -f docker-compose.renesas.yaml up -d
+docker compose -f docker-compose.renesas.yaml up -d --build
 
 docker image prune -f
